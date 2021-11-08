8b75c4776a83eb2015e2928d8c0a4f8903a78ebb
TadasIndriliunas <tadas.indriliunas@gmail.com>
2021-11-08 21:35:08 +0200

    [5]: save toys to state

--- js/components/ToyGridComponent.js
+++ js/components/ToyGridComponent.js
@@ -1,3 +1,33 @@
 class ToyGridComponent {
+  constructor() {
+    this.htmlElement = document.createElement('div');
+    this.state = {
+      toys: []
+    };
+    this.init();
+  }
+
+  init = () => {
+    API.getToys(this.saveToys, this.showError);
+    this.render();
+  }
+
+
+  showError = error => {
+    console.error(error);
+  }
   
+  saveToys = (toys) => {
+    this.state.toys = toys;
+    this.render();
+  }
+
+
+  render = () => {
+    if (this.state.toys.length === 0) {
+      this.htmlElement.innerHTML = 'NĖRA DUOMENŲ';
+    } else {
+      this.htmlElement.innerHTML = '<pre>' + JSON.stringify(this.state.toys) + '</pre>';
+    }
+  }
 }
