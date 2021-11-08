c6cb78141659c120944762fa6b852ecc881295d8
TadasIndriliunas <tadas.indriliunas@gmail.com>
2021-11-08 14:44:36 +0200

    [4] HTTP requests

---
+++ js/API.js
@@ -0,0 +1,32 @@
+const serverURL = 'http://localhost:3000';
+
+class API {
+  static getToy = (success, failure) => {
+    setTimeout(() => {
+      fetch(`${serverURL}/Toy`)
+        .then(res => res.json())
+        .then(success)
+        .catch(failure)
+    }, 1000);
+  }
+
+  static deleteToy = (id, success, failure) => {
+    fetch(`${serverURL}/Toy/${id}`, { method: 'DELETE' })
+      .then(res => res.json())
+      .then(success)
+      .catch(failure)
+  }
+}
+
+
+// API.getToy(
+//   (duomenys) => console.log('gavau duomenis', duomenys), // success
+//   (klaida) => console.error('klaida!!!!', klaida) // failure
+// )
+
+
+// API.deleteToy(
+//   '1', // id
+//   (duomenys) => console.log('gavau duomenis', duomenys), // success
+//   (klaida) => console.error('klaida!!!!', klaida) // failure
+// )
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const rootContainer = document.querySelector('#root');
+
+const toyGrid = new ToyGridComponent();
+
+rootContainer.append(toyGrid.htmlElement);
